f6013104a5d82b498ea5e414436d5ad48f2bf2d1
Siddhesh582 <69677649+Siddhesh582@users.noreply.github.com>
2025-03-20 13:47:07 -0400

    Update car_racing.py
    changed it to default environment rather than custom Car racing environment

--- car_racing.py
+++ car_racing.py
@@ -44,12 +44,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from main import CustomCarRacing
 
-print("[INFO] Creating CustomCarRacing environment...")
-env = CustomCarRacing(track_complexity=1.5, track_width=0.8, num_forks=2)
+# print("[INFO] Creating CustomCarRacing environment...")
+# env = CustomCarRacing(track_complexity=1.5, track_width=0.8, num_forks=2)
 
-print("[INFO] Resetting environment...")
-obs, info = env.reset()
-print("[INFO] Environment reset successful!")
+# print("[INFO] Resetting environment...")
+# obs, info = env.reset()
+# print("[INFO] Environment reset successful!")
+
+# env.close()
+# print("[INFO] Closed environment.")
+
+env = gym.make("CarRacing", render_mode="human")
+observation, info = env.reset()
+
+episode_over = False
+while not episode_over:
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    episode_over = terminated or truncated
 
 env.close()
-print("[INFO] Closed environment.")
